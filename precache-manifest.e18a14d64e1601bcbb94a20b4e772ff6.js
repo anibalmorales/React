78a973d615cbb5821d39f3fc304fbcd5be5394a5
Anibal Morales <anibal.ingenieria@gmail.com>
2020-08-24 07:02:30 -0600

    Updates

---
+++ precache-manifest.e18a14d64e1601bcbb94a20b4e772ff6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4739d46080917c53b5821caee9831084",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "71f35756554dcf5b600b",
+    "url": "/react/static/css/main.d5f73fd8.chunk.css"
+  },
+  {
+    "revision": "1d9cdb26beda46aae70a",
+    "url": "/react/static/js/2.99645e40.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/react/static/js/2.99645e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71f35756554dcf5b600b",
+    "url": "/react/static/js/main.2e2131c3.chunk.js"
+  },
+  {
+    "revision": "c13159b7ef8a33cf4084",
+    "url": "/react/static/js/runtime-main.d6e92acd.js"
+  }
+]);
